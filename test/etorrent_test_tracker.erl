b1f6700c80d18c7a09b15ad87b680cfe056b90cc
Emil Holmström <emil.holmstrom@gmail.com>
2026-05-31 16:11:12 -0700

    Add etorrent_test_tracker: minimal HTTP tracker for CT
    start/1 and stop/1 were called by the test suite but the module was
    never implemented. Uses gen_tcp directly so it works before cowboy is
    started by start_app/2. Stores peers in ETS and responds with a
    bencoded peer list.

---
+++ test/etorrent_test_tracker.erl
@@ -0,0 +1,99 @@
+%% Minimal HTTP BitTorrent tracker for Common Test suites.
+%%
+%% Uses gen_tcp directly so it can start before the cowboy application
+%% is brought up by start_app/2 in the test suite.
+-module(etorrent_test_tracker).
+-behaviour(gen_server).
+
+-export([start/1, stop/1]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
+
+-record(state, {lsock, peers}).
+
+start(Port) ->
+    gen_server:start({local, ?MODULE}, ?MODULE, Port, []).
+
+stop(Pid) ->
+    gen_server:stop(Pid).
+
+init(Port) ->
+    {ok, LSock} = gen_tcp:listen(Port, [binary, {reuseaddr, true}, {active, false}]),
+    Peers = ets:new(tracker_peers, [bag]),
+    self() ! accept,
+    {ok, #state{lsock = LSock, peers = Peers}}.
+
+handle_info(accept, #state{lsock = LSock, peers = Peers} = State) ->
+    case gen_tcp:accept(LSock, 50) of
+        {ok, Sock} ->
+            spawn(fun() -> handle_request(Sock, Peers) end),
+            self() ! accept;
+        {error, timeout} ->
+            self() ! accept;
+        {error, _} ->
+            ok
+    end,
+    {noreply, State};
+handle_info(_, State) ->
+    {noreply, State}.
+
+handle_call(_, _, State) -> {reply, ok, State}.
+handle_cast(_, State)    -> {noreply, State}.
+
+terminate(_, #state{lsock = LSock, peers = Peers}) ->
+    ets:delete(Peers),
+    gen_tcp:close(LSock),
+    ok.
+
+code_change(_, State, _) -> {ok, State}.
+
+handle_request(Sock, Peers) ->
+    case gen_tcp:recv(Sock, 0, 5000) of
+        {ok, Data} -> handle_http(Sock, Data, Peers);
+        _          -> gen_tcp:close(Sock)
+    end.
+
+handle_http(Sock, Data, Peers) ->
+    [RequestLine | _] = binary:split(Data, <<"\r\n">>),
+    case binary:split(RequestLine, <<" ">>, [global]) of
+        [<<"GET">>, Path, _] -> handle_announce(Sock, Path, Peers);
+        _                    -> respond(Sock, 400, <<"Bad Request">>)
+    end.
+
+handle_announce(Sock, Path, Peers) ->
+    case binary:split(Path, <<"?">>) of
+        [_, QS] ->
+            Params    = parse_qs(QS),
+            InfoHash  = proplists:get_value(<<"info_hash">>, Params, <<>>),
+            AnnPort   = binary_to_integer(
+                          proplists:get_value(<<"port">>, Params, <<"0">>)),
+            ets:insert(Peers, {InfoHash, <<"127.0.0.1">>, AnnPort}),
+            PeerList  = [[{<<"ip">>, Ip}, {<<"port">>, P}]
+                         || {_, Ip, P} <- ets:lookup(Peers, InfoHash)],
+            Body      = iolist_to_binary(etorrent_bcoding:encode(
+                            [{<<"interval">>, 30}, {<<"peers">>, PeerList}])),
+            respond(Sock, 200, Body);
+        _ ->
+            respond(Sock, 400, <<"Bad Request">>)
+    end.
+
+respond(Sock, Code, Body) ->
+    StatusText = case Code of 200 -> <<"OK">>; _ -> <<"Error">> end,
+    Len = integer_to_binary(byte_size(Body)),
+    Response = <<"HTTP/1.0 ", (integer_to_binary(Code))/binary, " ",
+                 StatusText/binary, "\r\nContent-Length: ", Len/binary,
+                 "\r\n\r\n", Body/binary>>,
+    gen_tcp:send(Sock, Response),
+    gen_tcp:close(Sock).
+
+parse_qs(QS) ->
+    [parse_param(P) || P <- binary:split(QS, <<"&">>, [global])].
+
+parse_param(P) ->
+    case binary:split(P, <<"=">>) of
+        [K, V] -> {percent_decode(K), percent_decode(V)};
+        [K]    -> {percent_decode(K), <<>>}
+    end.
+
+percent_decode(B) ->
+    uri_string:percent_decode(B).
